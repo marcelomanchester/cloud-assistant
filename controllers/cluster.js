3e61491103d793cc807dde387bbcce3a244a4f47
Marcelo Manchester <celoqueiroga@gmail.com>
2020-05-01 19:30:42 -0300

    cluster controller's scope

---
+++ controllers/cluster.js
@@ -0,0 +1,24 @@
+const async = require('async');
+
+const createCluster = (req, res) => {
+
+};
+
+const updateCluster = (req, res) => {
+
+};
+
+const indexCluster = (req, res) => {
+
+};
+
+const deleteCluster = (req, res) => {
+
+};
+
+module.exports = {
+  createCluster,
+  updateCluster,
+  indexCluster,
+  deleteCluster,
+};
